6b62b0a2e393c2ae6ed4c351ff5cb4f7f141c24d
sein <seinkane01@gmail.com>
2024-01-07 13:05:56 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const createTaskForm = document.getElementById("create-task-form");
+
+  createTaskForm.addEventListener("submit", (event) => {
+    
+    event.preventDefault();
+   const taskDescription = document.getElementById("new-task-description").value;
+   const newTask = document.createElement("li");
+    newTask.textContent = taskDescription;
+  
+    
+    const taskList = document.getElementById("task-list");
+    taskList.appendChild(newTask);
+  
+    
+    document.getElementById("new-task-description").value = "";
+  });
 });
+
+
+
